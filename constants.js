2cba13ef619799f9bd417185ea18c1e59d6f4b95
Muhammad Akbar <bonntt@gmail.com>
2024-06-03 20:03:00 +0200

    Removed .env and added example file

--- constants.js
+++ constants.js
@@ -1,5 +1,8 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 const dbHost = process.env.INFLUX_URL || 'http://localhost:8086';
-const dbToken = process.env.INFLUX_TOKEN || 'aL90HpXAfRkEBPPwjRO1CBlG-niYc_Nnilx2bFMn38p5XP2Cd1C73jSoAv0vinfg8I3azzEu-uzBl-i-v4TksQ==';
+const dbToken = process.env.INFLUX_TOKEN || '';
 const dbOrg = process.env.INFLUX_ORG || 'offgrid';
 const dbBucket = process.env.INFLUX_BUCKET || 'system_usage';
 
